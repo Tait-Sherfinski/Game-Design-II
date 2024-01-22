83c50c176635309d01ec4259582dc71dbdf8cf0e
Tait-Sherfinski <Tsherfin@gmail.com>
2024-01-22 15:18:02 -0600

    Initial commit

---
+++ Hello.gd
@@ -0,0 +1,17 @@
+extends Control
+
+
+func _ready():
+	pass
+
+
+func _process(delta):
+	pass
+
+
+func _on_button_pressed():
+	$Label.text = "Hello, world!"
+
+
+func _on_button_2_pressed():
+	$Label.text = ""
